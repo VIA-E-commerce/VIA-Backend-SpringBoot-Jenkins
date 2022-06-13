fd87fe36461fd3def7fb3a025896142848ceb682
m-dzn <88yangkh@gmail.com>
2022-06-13 15:21:07 +0900

    :tada: Setup SpringBoot Project

---
+++ src/test/java/com/mdzn/via/ViaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdzn.via;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
